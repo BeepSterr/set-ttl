fd8377c09a1b052e9aa7e698b29fa6726bb01bc4
BeepSterr <git@beeps.dev>
2022-01-09 11:36:51 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+module.exports = class SetTTL extends Set {
+
+    ttl_cache = new Map();
+    default_ttl= 0;
+
+    constructor(default_ttl) {
+        super();
+        this.default_ttl = Number(default_ttl);
+    }
+
+    #checkTTL(){
+
+        const now = new Date().getTime();
+        for(const [key, value] of this.ttl_cache){
+            if(value <= now){
+                this.delete(key);
+                this.ttl_cache.delete(key);
+            }
+        }
+
+    }
+
+    add(v, ttl){
+        const current = new Date().getTime();
+        this.ttl_cache.set(v, ttl ? current + ttl : current + this.default_ttl)
+        return super.add(v);
+    }
+
+    extend(v, ttl){
+        const current = this.ttl_cache.get(v);
+        if(current){
+            this.ttl_cache.set(v, ttl ? current + ttl : current + this.default_ttl)
+        }
+    }
+
+    clear(){
+        this.ttl_cache = new Map();
+        return super.clear();
+    }
+
+    delete(v){
+        this.ttl_cache.delete(v);
+        return super.delete(v);
+    }
+
+    has(v){
+        this.#checkTTL.call(this);
+        return super.has(v);
+    }
+
+    get size(){
+        this.#checkTTL.call(this);
+        return super.size;
+    }
+}
+
+
---
+++ test.js
@@ -0,0 +1,25 @@
+const setttl = require('./index');
+
+
+    // Create cache with a TTL of 500ms
+    let cache = new setttl(500);
+
+    // Add a value to the set, TTL is not specified so the default is used.
+    cache.add('test 1');
+
+    // Check the contents of "test 1" after 250ms
+    setTimeout( ()=> {
+        console.log('Should be true: ', cache.has('test 1'));
+        cache.extend('test 1', 300); //Extends cached value TTL by 300ms
+    }, 250)
+
+    // Check the contents of "test 1" after 250ms
+    // Item should still exists, cause it was extended at 250ms
+    setTimeout( ()=> {
+        console.log('Should be true: ', cache.has('test 1'));
+    }, 500)
+
+    // Now a second has passed and the 500+300ms TTL should have passed.
+    setTimeout( ()=> {
+        console.log('Should be false: ', cache.has('test 1'));
+    }, 1000)
